da465b4a4c36d01f92992a8166761e81e42d40f2
Tejasvi <tejasvijain09@gmail.com>
2026-06-07 09:53:33 +0530

    Initial Node.js project setup

---
+++ test.js
@@ -0,0 +1,7 @@
+const add = require("./index");
+
+if(add(2,3) === 5){
+    console.log("Test passed: add(2, 3) is equal to 5");
+}else{
+    throw new Error("Test failed: add(2, 3) is not equal to 5");
+}
