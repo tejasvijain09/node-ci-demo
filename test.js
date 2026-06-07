38a8758fc4aee1062f63f08f736b92d045886529
Tejasvi <tejasvijain09@gmail.com>
2026-06-07 10:03:30 +0530

    Fixed syntax errors

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const add = require("./index");
 
-if(add(2,3) === 5){
-    console.log("Test passed: add(2, 3) is equal to 5");
+if(add(10,20) === 30){
+    console.log("Test passed: add(10, 20) is equal to 30");
 }else{
-    throw new Error("Test failed: add(2, 3) is not equal to 5");
+    throw new Error("Test failed: add(10, 20) is not equal to 30");
 }
